ac6c247121ba6c556ef7680b40fca5d1264d4890
Pedro Huerta <77652963+phuertay@users.noreply.github.com>
2026-02-22 14:25:46 -0700

    Add zmk_usb_hid_send_report function declaration

--- app/include/zmk/usb_hid.h
+++ app/include/zmk/usb_hid.h
@@ -14,4 +14,6 @@ int zmk_usb_hid_send_consumer_report(void);
 int zmk_usb_hid_send_mouse_report(void);
 #endif // IS_ENABLED(CONFIG_ZMK_POINTING)
 void zmk_usb_hid_set_protocol(uint8_t protocol);
+#if IS_ENABLED(CONFIG_ZMK_PLOVER_HID)
 int zmk_usb_hid_send_report(const uint8_t *report, size_t len);
+#endif /* IS_ENABLED(CONFIG_ZMK_PLOVER_HID) */
